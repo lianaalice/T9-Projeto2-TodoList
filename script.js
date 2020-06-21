5f4a6c06350a0c8b0cbeecc5739087397b244ead
liana alice <lyannalisse@gmail.com>
2020-06-21 18:53:03 -0300

    revendo projeto 2

--- script.js
+++ script.js
@@ -12,70 +12,84 @@
 // - Excluir todos os itens da lista  ----checked
 // - Reorganizar a ordem "arrastando" os cards utilizando drag and drop
 
+window.addEventListener("DOMContentLoaded", function () {
+  const lista = document.querySelector("#todoLista");
+  const inputText = document.querySelector("#todoInput");
+  const inputSubmit = document.querySelector("#todoSubmit");
 
-window.addEventListener('DOMContentLoaded', function(){
+  const btnMarcar = document.querySelector("#todoMarcarTodos");
+  const btnRemover = document.querySelector("#todoRemoverTodos");
 
-const lista = document.querySelector('#todoLista')
-const inputText = document.querySelector('#todoInput')
-const inputSubmit = document.querySelector('#todoSubmit')
+  //função add item
+  function addLista(itemLista) {
+    itemLista.innerText = inputText.value;
+  }
 
-//const tarefas = document.querySelector('#todoTarefas')
+  //função exclui item
+  function excluirLista(boxLista) {
+    lista.removeChild(boxLista);
+  }
 
-//const divLista = document.querySelector('.todo__tarefas-lista')
+  //função marcar todos itens
+  function marcarTodosItems (itemLista){
+    itemLista.classList.add("checked");
+  }
 
-const btnMarcar = document.querySelector('#todoMarcarTodos')
-const btnRemover = document.querySelector('#todoRemoverTodos')
+//evento geral -
+  inputSubmit.addEventListener("click", (event) => {
+    //previnindo comportamento padrão do click no submit e atualização imediata da pagina ao click
+    event.preventDefault();
 
+    //criando lista. Igualando valor escrito no input ao texto da 'li'
+    const boxLista = document.createElement("span");
+    const itemLista = document.createElement("li");
 
+    let btnLista = document.createElement("button");
+    btnLista.innerText = "X";
 
+    //estilo do span boxLista
+    boxLista.style.display = "flex";
+    boxLista.style.justifyContent = "space-between";
 
-inputSubmit.addEventListener('click', (event) => {
-    event.preventDefault()
-    let boxLista = document.createElement('span')
-    
-    let itemLista = document.createElement('li')
-    let btnLista = document.createElement('button')
-    btnLista.innerText = 'X'
-    
-    itemLista.innerText = inputText.value
-    
+    //dando parentesco, add itens sobre itens
+    boxLista.appendChild(itemLista);
+    boxLista.appendChild(btnLista);
+    lista.appendChild(boxLista);
+
+    //function add
+    addLista(itemLista);
+ 
     // - Validar campo de texto para não entrar itens vazios
-    if (inputText.value.trim() == ""){
-        alert("Tem que pôr uma tarefa pra funcionar o bagulho, bem");
-        return false;
+    if (inputText.value.trim() == "") {
+      alert("Tem que pôr uma tarefa pra funcionar a lista, bem");
+      return false;
     }
-    // limpar campos de texto depois
-    if(inputText.value > "") {
-        inputText.value = ""
-    } 
-    
-    boxLista.style.display = 'flex'
-    boxLista.style.justifyContent = 'space-between'
-    
-    boxLista.appendChild(itemLista)
-    boxLista.appendChild(btnLista)
-    lista.appendChild(boxLista)
-    
-    btnLista.addEventListener('click', () => {
-        lista.removeChild(boxLista)
-    })
-    
-    itemLista.addEventListener('click', () => {
-        itemLista.classList.toggle('checked')
-        itemLista.classList.toggle('over')
-    })
+ 
+    // limpar campos de texto depois de usado
+    if (inputText.value > "") {
+      inputText.value = "";
+    }
 
-    btnRemover.addEventListener('click', function(){
-        lista.removeChild(boxLista)
+    //remove individual event and function
+    btnLista.addEventListener("click", () => {
+      lista.removeChild(boxLista);
+    });
 
-    })
-
-    btnMarcar.addEventListener('click', function(){
-        itemLista.classList.toggle('checked')
-        itemLista.classList.toggle('over')    
-    })
+    //checklist individual event and function
+    itemLista.addEventListener("click", () => {
+      itemLista.classList.toggle("checked");
+    });
 
 
-})
+    //eventos dos btn
+    btnMarcar.addEventListener("click", function () {
+    marcarTodosItems (itemLista);
+});
 
-})
+    btnRemover.addEventListener("click", function () {
+      excluirLista(boxLista);
+    });
+});
+
+
+});
